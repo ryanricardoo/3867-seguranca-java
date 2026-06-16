f473f8d4190e5e83a15c432f67c4032b78ced2c8
Ryan Ricardo <ricardoryan2403@gmail.com>
2026-06-16 12:43:56 -0300

    git commit -m "feat: associa tópico dinamicamente ao usuário autenticado"

--- src/main/java/br/com/forum_hub/domain/topico/DadosCadastroTopico.java
+++ src/main/java/br/com/forum_hub/domain/topico/DadosCadastroTopico.java
@@ -4,9 +4,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record DadosCadastroTopico(
-         @NotBlank String titulo,
-         @NotBlank String mensagem,
-         @NotBlank String autor,
-         @NotNull Long cursoId
+        @NotBlank String titulo,
+        @NotBlank String mensagem,
+        @NotNull Long cursoId
 ) {
 }
