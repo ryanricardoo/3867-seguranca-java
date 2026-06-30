f1d1a1f5cf0b1c35ddae6d8d5f24abacc0120e74
Ryan Ricardo <ricardoryan2403@gmail.com>
2026-06-30 17:35:17 -0300

    feat(usuario): adicionar suporte à verificação de conta por e-mail

--- src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
+++ src/main/java/br/com/forum_hub/domain/usuario/UsuarioRepository.java
@@ -2,10 +2,10 @@ package br.com.forum_hub.domain.usuario;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
+
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    Optional<Usuario> findByEmailIgnoreCase(String email);
+    Optional<Usuario> findByEmailIgnoreCaseAndVerificadoTrue(String email);
     Optional<Usuario> findByRefreshToken(String refreshToken);
 }
--- src/main/java/br/com/forum_hub/infra/seguranca/FiltroTokenAcesso.java
+++ src/main/java/br/com/forum_hub/infra/seguranca/FiltroTokenAcesso.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -33,7 +32,7 @@ public class FiltroTokenAcesso extends OncePerRequestFilter {
 
         if(token != null){
             String email = tokenService.verificarToken(token);
-            Usuario usuario = usuarioRepository.findByEmailIgnoreCase(email).orElseThrow();
+            Usuario usuario = usuarioRepository.findByEmailIgnoreCaseAndVerificadoTrue(email).orElseThrow();
 
             Authentication authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
